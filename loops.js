5ab288509877189c73c9a5b5a224370c149703c6
searching-form-5732 <kevin.downs04@gmail.com>
2019-09-15 09:18:38 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -20,16 +20,16 @@ function whileLoop(num){
 
 
 
-function incrementVariable() {
-  i = i + 1;
-  return i;
-}
+
 
 function doWhileLoop(int){
   var i = 0;
-  
+  function incrementVariable() {
+  i = i + 1;
+  return i;
+}
   
   do{
     console.log("I run once regardless.")
-  } while (incrementVariable() <= int)
+  } while (incrementVariable() < int)
 }
